a8f683ae4e6d02bb9fed0da65629b9122158ec5e
RedCreator37 <tobija.zuntar@gmail.com>
2019-11-17 12:22:12 +0100

    Fix the logo, add iso folder to .gitignore

--- kernel.cpp
+++ kernel.cpp
@@ -8,11 +8,13 @@
 int main() {
     init_terminal();
 
-    out_string("        _   _________  ____________  _____         ___  \n");
+	terminal_setcolor(VGA_WHITE, VGA_BLACK);
+    out_string("    _   _________  ____________   _____        ___  \n");
     out_string("   / | / / ____/ |/ /_  __/ __ \\/ ___/   _   _<  /     \n");
     out_string("  /  |/ / __/  |   / / / / / / /\\__ \\   | | / / /     \n");
     out_string(" / /|  / /___ /   | / / / /_/ /___/ /   | |/ / /        \n");
-    out_string("/_/ |_/_____//_/|_|/_/  \\____//____/    |___/_/        \n");
+    out_string("/_/ |_/_____//_/|_|/_/  \\____//____/    |___/_/        \n\n");
+	out_string("Starting NEXTOS v1.0... Hold on...\n");
 
     // print the hello world message
     terminal_setcolor(VGA_YELLOW, VGA_BLACK);
