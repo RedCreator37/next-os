6697796f55c0a57dc5450b2ed7caea45b54fc94c
RedCreator37 <tobija.zuntar@gmail.com>
2019-11-17 10:25:44 +0100

    An attempt to improve the standard functionality (again)

--- kernel.cpp
+++ kernel.cpp
@@ -2,111 +2,40 @@
 // kernel.cpp - main source file
 // 2019-11-16 by RedCreator37
 #include "vgadef.hpp"
-/*
-unsigned short *terminal_buffer;
-unsigned int vga_index;
 
+unsigned short *terminal_buffer;
+//unsigned int vga_index;
+unsigned long terminal_row;
+unsigned long terminal_column;
+unsigned char terminal_color;
+
+// screen handling
 void clr_scr() {
     int index = 0;
     // 25 lines by 80 columns, each element takes 2 bytes
-    while (index < 80 * 25 * 2) {
+    while (index < VGA_WIDTH * VGA_HEIGHT * 2) {
         terminal_buffer[index] = ' ';
         index += 2;
     }
 }
 
-void print_str(char *str, unsigned char color) {
-    int index = 0;
-    while (str[index]) {
-        terminal_buffer[vga_index] = (unsigned short)str[index] | (unsigned short)color << 8;
-        index++;
-        vga_index++;
-    }
-}
-
-int main() {
-    // todo: change
-    terminal_buffer = (unsigned short *)VGA_ADDRESS;
-    vga_index = 0;
-
-    clr_scr();
-    print_str("Hello World! This is a custom operating system kernel, written by RedCreator37.", YELLOW);
-    vga_index = 80;     // next line
-    print_str("Status: ", YELLOW);
-    vga_index = 89;
-    print_str("OK", GREEN);
-    vga_index = 160;
-    print_str("Goodbye!", BRIGHT_RED);
-    return 0;
-}
-*/
-
-
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-
-/* Hardware text mode color constants. */
-enum vga_color {
-	VGA_COLOR_BLACK = 0,
-	VGA_COLOR_BLUE = 1,
-	VGA_COLOR_GREEN = 2,
-	VGA_COLOR_CYAN = 3,
-	VGA_COLOR_RED = 4,
-	VGA_COLOR_MAGENTA = 5,
-	VGA_COLOR_BROWN = 6,
-	VGA_COLOR_LIGHT_GREY = 7,
-	VGA_COLOR_DARK_GREY = 8,
-	VGA_COLOR_LIGHT_BLUE = 9,
-	VGA_COLOR_LIGHT_GREEN = 10,
-	VGA_COLOR_LIGHT_CYAN = 11,
-	VGA_COLOR_LIGHT_RED = 12,
-	VGA_COLOR_LIGHT_MAGENTA = 13,
-	VGA_COLOR_LIGHT_BROWN = 14,
-	VGA_COLOR_WHITE = 15,
-};
- 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)  {
+// compiles the color values
+unsigned char vga_put_color(int fg, int bg)  {
 	return fg | bg << 4;
 }
- 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color)  {
-	return (uint16_t) uc | (uint16_t) color << 8;
-}
- 
-size_t strlen(const char* str)  {
-	size_t len = 0;
-	while (str[len]) len++;
-	return len;
-}
- 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
- 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
- 
-void terminal_initialize(void)  {
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
-		}
-	}
-}
- 
-void terminal_setcolor(uint8_t color) {
+
+// set the color
+void terminal_setcolor(unsigned char color) {
 	terminal_color = color;
 }
- 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)  {
-	const size_t index = y * VGA_WIDTH + x;
+
+// put together the character
+unsigned short vga_entry(unsigned char uc, unsigned char color)  {
+	return (unsigned short)uc | (unsigned short)color << 8;
+}
+
+void terminal_putentryat(char c, unsigned char color, unsigned long x, unsigned long y)  {
+	const unsigned long index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
  
@@ -118,21 +47,53 @@ void terminal_putchar(char c)  {
 			terminal_row = 0;
 	}
 }
- 
-void terminal_write(const char* data, size_t size)  {
-	for (size_t i = 0; i < size; i++)
+
+unsigned long strlen(const char* str) {
+	unsigned long len = 0;
+	while (str[len]) len++;
+	return len;
+}
+
+void terminal_write(const char* data, unsigned long size)  {
+	for (unsigned long i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
  
 void terminal_writestring(const char* data)  {
 	terminal_write(data, strlen(data));
 }
- 
-int main()  {
-	/* Initialize terminal interface */
-	terminal_initialize();
- 
-	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+
+// deprecated
+//void print_str(char *str, unsigned char color) {
+//    int index = 0;
+//    while (str[index]) {
+//        terminal_buffer[vga_index] = (unsigned short)str[index] | (unsigned short)color << 8;
+//        index++;
+//        vga_index++;
+//    }
+//}
+
+void init_terminal() {
+    terminal_row = 0;
+    terminal_column = 0;
+    terminal_color = vga_put_color(VGA_YELLOW, VGA_BLACK);
+    terminal_buffer = (unsigned short *)VGA_ADDRESS;
+    clr_scr();
+}
+
+int main() {
+    // todo: change
+    init_terminal();
+
+    terminal_writestring("Hello World! This is a custom os kernel!\nHello Again!");
+
+    //vga_index = 0;
+    //print_str("Hello World! This is a custom operating system kernel, written by RedCreator37.", VGA_YELLOW);
+    //vga_index = 80;     // next line
+    //print_str("Status: ", VGA_YELLOW);
+    //vga_index = 89;
+    //print_str("OK", VGA_GREEN);
+    //vga_index = 160;
+    //print_str("Goodbye!", VGA_BRIGHT_RED);
     return 0;
 }
--- vgadef.hpp
+++ vgadef.hpp
@@ -5,20 +5,26 @@
 // video memory begins here
 #define VGA_ADDRESS 0xB8000
 
+// display properties
+static const unsigned long VGA_WIDTH = 80;
+static const unsigned long VGA_HEIGHT = 25;
+
 // all 16 VGA colors
-#define BLACK          0x0
-#define BLUE           0x1
-#define GREEN          0x2
-#define CYAN           0x3
-#define RED            0x4
-#define MAGENTA        0x5
-#define BROWN          0x6
-#define GRAY           0x7
-#define DARK_GRAY      0x8
-#define BRIGHT_BLUE    0x9
-#define BRIGHT_GREEN   0xA
-#define BRIGHT_CYAN    0xB
-#define BRIGHT_RED     0xC
-#define BRIGHT_MAGENTA 0xD
-#define YELLOW         0xE
-#define WHITE          0xF
+enum VGA_COLOR {
+    VGA_BLACK          = 0x0,
+    VGA_BLUE           = 0x1,
+    VGA_GREEN          = 0x2,
+    VGA_CYAN           = 0x3,
+    VGA_RED            = 0x4,
+    VGA_MAGENTA        = 0x5,
+    VGA_BROWN          = 0x6,
+    VGA_GRAY           = 0x7,
+    VGA_DARK_GRAY      = 0x8,
+    VGA_BRIGHT_BLUE    = 0x9,
+    VGA_BRIGHT_GREEN   = 0xA,
+    VGA_BRIGHT_CYAN    = 0xB,
+    VGA_BRIGHT_RED     = 0xC,
+    VGA_BRIGHT_MAGENTA = 0xD,
+    VGA_YELLOW         = 0xE,
+    VGA_WHITE          = 0xF
+};
